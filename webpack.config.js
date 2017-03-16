1b367182e96b188cf634b6e43255da4188dc6ba8
Andrew Page <andrew.page32@gmail.com>
2017-03-16 12:57:17 -0400

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 var path = require('path');
 
 module.exports = {
- entry: './src/index.tsx',
- output: {
-   filename: 'bundle.js',
-   path: path.resolve(__dirname, 'dist')
- },
- module: {
-   rules: [
-     {
-       enforce: 'pre',
-       test: /\.js$/,
-       loader: "source-map-loader"
-     },
-     {
-       enforce: 'pre',
-       test: /\.tsx?$/,
-       use: "source-map-loader"
-     },
-     {
-       test: /\.tsx?$/,
-       loader: 'ts-loader',
-       exclude: /node_modules/,
-     },
-   ]
- },
- resolve: {
-   extensions: [".tsx", ".ts", ".js"]
- },
- devtool: 'inline-source-map',
- // When importing a module whose path matches one of the following, just
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        use: "source-map-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9000
+  },
+  // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
