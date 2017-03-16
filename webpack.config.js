35586516f961849d6cfe764b9ef69062fc09d2dd
Andrew Page <andrew.page32@gmail.com>
2017-03-16 11:41:09 -0400

    React, webpack, and typescript setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+var path = require('path');
 
 module.exports = {
-  entry: [
-    './app/index.js'
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-    ]
+ entry: './src/index.tsx',
+ output: {
+   filename: 'bundle.js',
+   path: path.resolve(__dirname, 'dist')
+ },
+ module: {
+   rules: [
+     {
+       enforce: 'pre',
+       test: /\.js$/,
+       loader: "source-map-loader"
+     },
+     {
+       enforce: 'pre',
+       test: /\.tsx?$/,
+       use: "source-map-loader"
+     },
+     {
+       test: /\.tsx?$/,
+       loader: 'ts-loader',
+       exclude: /node_modules/,
+     },
+   ]
+ },
+ resolve: {
+   extensions: [".tsx", ".ts", ".js"]
+ },
+ devtool: 'inline-source-map',
+ // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
   },
-  output: {
-    filename: 'index_bundle.js',
-    path: __dirname + '/dist'
-  },
-  plugins: [
-    HtmlWebpackPluginConfig
-  ]
 };
